3363273c2b2f4e39d8dcdcb78c4b03be79f231c4
bearjen <bearjen.2880@gmail.com>
2026-01-08 16:37:43 +0800

    Delete services/services directory

--- services/services/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabaseUrl = 'https://rtzwvdwsyupkbuovzkhk.supabase.co'
-export const supabaseKey = 'sb_publishable_kb38CrjY3PFE7SGW3_Djjg_M9vwzitL' // 這裡要填完整的長亂碼
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
